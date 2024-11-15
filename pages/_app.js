a49bbdfb7f26a390d3ef8e07ead631b2d0aefb53
Jennifer Vobis <jennifervobis@Jen.local>
2024-11-15 11:59:01 +0000

    feat: app and index files

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '@styles/globals.css'
+import 'dist/styles.css'
 
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,72 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
+import Header from 'modules/Header';
+import Footer from 'modules/Footer';
+import Hello from 'modules/Hello';
+import Projects from 'modules/Projects';
+import ProjectsTitle from 'modules/ProjectsTitle';
+import Contact from 'modules/Contact';
+import Navigation from 'modules/Navigation';
+import { gsap, ScrollTrigger } from "../lib/gsap";
+import { SectionProvider } from '../lib/sectionContext';
+import projects from '../lib/projects.json';
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    const [activeSection, setActiveSection] = useState('hello');
+    const [availableSections, setAvailableSections] = useState([]);
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+    useEffect(() => {
 
-      <Footer />
-    </div>
-  )
-}
+      const sections = gsap.utils.toArray('.section');
+
+      // Initialize ScrollTriggers
+      sections.forEach((section) => {
+        ScrollTrigger.create({
+          trigger: section,
+          start: 'top center',
+          end: 'bottom center',
+          // markers: true,
+          onEnter: () => setActiveSection(section.getAttribute('data-id')),
+          onEnterBack: () => setActiveSection(section.getAttribute('data-id')),
+          onLeave: () => {
+            const nextSection = sections[sections.indexOf(section) + 1];
+            if (nextSection) setActiveSection(nextSection.getAttribute('data-id'));
+          },
+          onLeaveBack: () => {
+            const prevSection = sections[sections.indexOf(section) - 1];
+            if (prevSection) setActiveSection(prevSection.getAttribute('data-id'));
+          },
+        });
+        setAvailableSections(sections.map((section) => section.getAttribute('data-id')));
+      });
+
+      return () => {
+        ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+      };
+    }, []);
+
+    return (
+      <SectionProvider>
+          <div className='bg-yellow overflow-hidden'>
+              <Head>
+                  <title>Jen Vobis | Web Developer</title>
+                  <link rel="icon" href="/favicon.ico" />
+              </Head>
+
+              <main className='group/controller relative' data-active-section={activeSection}>
+                  <Header title="Jen Vobis | Web Developer" />
+                  <Hello />
+                  <div className='relative'>
+                      <ProjectsTitle />
+                      <Projects projects={projects} />
+                  </div>
+                  <Navigation activeSection={activeSection} availableSections={availableSections} />
+              </main>
+
+              <Footer />
+          </div>
+      </SectionProvider>
+    );
+  }

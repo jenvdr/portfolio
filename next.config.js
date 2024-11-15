28325eb538a9f8265cd3a10df23bd9f7b360a016
Jennifer Vobis <jennifervobis@Jen.local>
2024-11-15 11:57:02 +0000

    build: revised dependencies and next config including redirect

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true
+module.exports = {
+    reactStrictMode: true,
+    swcMinify: true,
+    async redirects() {
+        return [
+        {
+            source: '/projects',
+            destination: '/',
+            permanent: true,
+        },
+        ]
+    },
 };
-
-module.exports = nextConfig;
